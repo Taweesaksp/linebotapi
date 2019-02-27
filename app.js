41a5585c72e7f5da85fecfefa700a2ddfbd324c1
Taweesaksp <34016386+Taweesaksp@users.noreply.github.com>
2019-02-27 16:09:40 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-// const express = require('express')
-// const app = express()
-// const port = process.env.PORT || 4000
-// app.post('/webhook', (req, res) => res.sendStatus(200))
-// app.listen(port)
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000
+app.post('/webhook', (req, res) => res.sendStatus(200))
+app.listen(port)
 
-/**
- * @author Pamontep Panya
- * @email pamontep.p@gmail.com
- * @create date 2018-06-03 03:45:02
- * @modify date 2018-06-03 03:45:02
- * @desc A sample project of Node.js and Line API
-*/
-const server = require('express');
-const PORT = process.env.PORT || 4000;
-const request = require('request');
-const bodyParser = require('body-parser');
+// /**
+//  * @author Pamontep Panya
+//  * @email pamontep.p@gmail.com
+//  * @create date 2018-06-03 03:45:02
+//  * @modify date 2018-06-03 03:45:02
+//  * @desc A sample project of Node.js and Line API
+// */
+// const server = require('express');
+// const PORT = process.env.PORT || 4000;
+// const request = require('request');
+// const bodyParser = require('body-parser');
 
-server()
-    .use(bodyParser.json())
-    .use(bodyParser.urlencoded({ extended: false }))
-    .get('/', (req, res) => res.send(`Hi there! This is a nodejs-line-api running on PORT: ${PORT}`))
-    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+// server()
+//     .use(bodyParser.json())
+//     .use(bodyParser.urlencoded({ extended: false }))
+//     .get('/', (req, res) => res.send(`Hi there! This is a nodejs-line-api running on PORT: ${PORT}`))
+//     .listen(PORT, () => console.log(`Listening on ${PORT}`));
