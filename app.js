ea4d795ec9288ad98f24f5f9a8b04021dfcb9c66
Taweesaksp <34016386+Taweesaksp@users.noreply.github.com>
2019-02-28 13:18:10 +0700

    Update app.js

--- app.js
+++ app.js
@@ -23,56 +23,37 @@
 //     .get('/', (req, res) => res.send(`Hi there! This is a nodejs-line-api running on PORT: ${PORT}`))
 //     .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-var express = require('express')
-var bodyParser = require('body-parser')
-var request = require('request')
-var app = express()
-
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+const port = process.env.PORT || 4000
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-
-app.set('port', (process.env.PORT || 4000))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-
 app.post('/webhook', (req, res) => {
-    var text = req.body.events[0].message.text
-    var sender = req.body.events[0].source.userId
-    var replyToken = req.body.events[0].replyToken
-    console.log(text, sender, replyToken)
-    console.log(typeof sender, typeof text)
-    // console.log(req.body.events[0])
-    if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
-        sendText(sender, text)
-    }
+    let reply_token = req.body.events[0].replyToken
+    let msg = req.body.events[0].message.text
+    reply(reply_token, msg)
     res.sendStatus(200)
 })
-
-function sendText(sender, text) {
-    let data = {
-        to: sender,
-        messages: [
-            {
-                type: 'text',
-                text: 'สวัสดีค่ะ เราเป็นผู้ช่วยปรึกษาด้านความรัก สำหรับหมามิ้น 💞'
-            }
-        ]
+app.listen(port)
+function reply(reply_token, msg) {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer {UfC+kvnTY/FnCX4xlcvUS6rJpw5mPeqHw8inmF+He1FKVxAYvpo3yzIlpajMLq/nhi0j/w+P+nez4OKZtn0Wdd5uVTi7oQDPVCl/WbxpNlu4/rq9ZtSW4xCaChY9ZQCv6IZHznLJLFNoOD4j9CuM1gdB04t89/1O/w1cDnyilFU=}'
     }
-    request({
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer {UfC+kvnTY/FnCX4xlcvUS6rJpw5mPeqHw8inmF+He1FKVxAYvpo3yzIlpajMLq/nhi0j/w+P+nez4OKZtn0Wdd5uVTi7oQDPVCl/WbxpNlu4/rq9ZtSW4xCaChY9ZQCv6IZHznLJLFNoOD4j9CuM1gdB04t89/1O/w1cDnyilFU=}'
-        },
-        url: 'https://api.line.me/v2/bot/message/push',
-        method: 'POST',
-        body: data,
-        json: true
-    }, function (err, res, body) {
-        if (err) console.log('error')
-        if (res) console.log('success')
-        if (body) console.log(body)
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [{
+            type: 'text',
+            text: msg
+        }]
     })
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode);
+    });
 }
-
-app.listen(app.get('port'), function () {
-    console.log('run at port', app.get('port'))
-})
