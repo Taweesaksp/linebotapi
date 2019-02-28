d00149c620c698594952c712e2f0d54c18330e48
Taweesaksp <34016386+Taweesaksp@users.noreply.github.com>
2019-02-28 11:27:34 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,39 +1,22 @@
-// Echo reply
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
-const app = express()
-const port = process.env.PORT || 4000
+app.use(cors())
+app.use(bodyParser.json())
+
+app.set('port', (process.env.PORT || 4000))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.post('/webhook', (req, res) => {
-    let reply_token = req.body.events[0].replyToken
-    let msg = req.body.events[0].message.text
-    reply(reply_token, msg)
-    res.sendStatus(200)
+
+
+app.get('/', function (req, res) {
+    res.send('Hello')
 })
-app.listen(port)
-function reply(reply_token, msg) {
-    let headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer {UfC+kvnTY/FnCX4xlcvUS6rJpw5mPeqHw8inmF+He1FKVxAYvpo3yzIlpajMLq/nhi0j/w+P+nez4OKZtn0Wdd5uVTi7oQDPVCl/WbxpNlu4/rq9ZtSW4xCaChY9ZQCv6IZHznLJLFNoOD4j9CuM1gdB04t89/1O/w1cDnyilFU=}'
-    }
-    let body = JSON.stringify({
-        replyToken: reply_token,
-        messages: [{
-            type: 'text',
-            text: msg
-        }]
-    })
-    request.post({
-        url: 'https://api.line.me/v2/bot/message/reply',
-        headers: headers,
-        body: body
-    }, (err, res, body) => {
-        console.log('status = ' + res.statusCode);
-    });
-}
+
+app.listen(app.get('port'), function () {
+    console.log('run at port', app.get('port'))
+})
 
 // /**
 //  * @author Pamontep Panya
