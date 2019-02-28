873df4f34ac7e3eee1424149d68a49e47ea89c2b
Taweesaksp <34016386+Taweesaksp@users.noreply.github.com>
2019-02-28 13:03:01 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const request2 = require('request')
-const app = express()
-const port = process.env.PORT || 4000
-app.post('/webhook', (req, res) => res.sendStatus(200))
-app.listen(port)
+// const express = require('express')
+// const request2 = require('request')
+// const app = express()
+// const port = process.env.PORT || 4000
+// app.post('/webhook', (req, res) => res.sendStatus(200))
+// app.listen(port)
 
 // /**
 //  * @author Pamontep Panya
@@ -22,3 +22,41 @@ app.listen(port)
 //     .use(bodyParser.urlencoded({ extended: false }))
 //     .get('/', (req, res) => res.send(`Hi there! This is a nodejs-line-api running on PORT: ${PORT}`))
 //     .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+const port = process.env.PORT || 4000
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.post('/webhook', (req, res) => {
+    let reply_token = req.body.events[0].replyToken
+    reply(reply_token)
+    res.sendStatus(200)
+})
+app.listen(port)
+function reply(reply_token) {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer {xxxxxxx}'
+    }
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [{
+            type: 'text',
+            text: 'Hello'
+        },
+        {
+            type: 'text',
+            text: 'How are you?'
+        }]
+    })
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode);
+    });
+}
