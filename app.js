15b4e3508fa3fb50dd3b573724229be66227fbb3
Taweesaksp <34016386+Taweesaksp@users.noreply.github.com>
2019-02-28 11:46:02 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-var request = require('request')
+const request = require('request')
 const port = process.env.PORT || 4000
 app.post('/webhook', (req, res) => res.sendStatus(200))
 app.listen(port)
