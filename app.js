ac87474ab5b0d1e2219711c2be4fab87825b9036
Taweesaksp <34016386+Taweesaksp@users.noreply.github.com>
2019-02-28 15:15:55 +0700

    Update app.js

--- app.js
+++ app.js
@@ -23,74 +23,74 @@
 //     .get('/', (req, res) => res.send(`Hi there! This is a nodejs-line-api running on PORT: ${PORT}`))
 //     .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-// const express = require('express')
-// const bodyParser = require('body-parser')
-// const request = require('request')
-// const app = express()
-// const port = process.env.PORT || 4000
-// app.use(bodyParser.urlencoded({ extended: false }))
-// app.use(bodyParser.json())
-// app.post('/webhook', (req, res) => {
-//     let reply_token = req.body.events[0].replyToken
-//     let msg = req.body.events[0].message.text
-//     reply(reply_token, msg)
-//     res.sendStatus(200)
-// })
-// app.listen(port)
-// function reply(reply_token, msg) {
-//     let headers = {
-//         'Content-Type': 'application/json',
-//         'Authorization': 'Bearer {UfC+kvnTY/FnCX4xlcvUS6rJpw5mPeqHw8inmF+He1FKVxAYvpo3yzIlpajMLq/nhi0j/w+P+nez4OKZtn0Wdd5uVTi7oQDPVCl/WbxpNlu4/rq9ZtSW4xCaChY9ZQCv6IZHznLJLFNoOD4j9CuM1gdB04t89/1O/w1cDnyilFU=}'
-//     }
-//     let body = JSON.stringify({
-//         replyToken: reply_token,
-//         messages: [{
-//             type: 'text',
-//             text: msg
-//         }]
-//     })
-//     request.post({
-//         url: 'https://api.line.me/v2/bot/message/reply',
-//         headers: headers,
-//         body: body
-//     }, (err, res, body) => {
-//         console.log('status = ' + res.statusCode);
-//     });
-// }
-
-
+const express = require('express')
+const bodyParser = require('body-parser')
 const request = require('request')
+const app = express()
 const port = process.env.PORT || 4000
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
-    var text = req.body.events[0].message.text;
-    var userId = req.body.events[0].source.userId;
-    if (text == "Hello") {
-        var msg = [{
-            type: 'text',
-            text: 'สวสัดี'
-        }]; PushMessage(userId, text, msg);
-    } res.sendStatus(200);
-});
+    let reply_token = req.body.events[0].replyToken
+    let msg = req.body.events[0].message.text
+    reply(reply_token, msg)
+    res.sendStatus(200)
+})
 app.listen(port)
-
-function PushMessage(userId, text, msg) {
-    let data = {
-        to: userId,
-        messages: msg
+function reply(reply_token, msg) {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer {UfC+kvnTY/FnCX4xlcvUS6rJpw5mPeqHw8inmF+He1FKVxAYvpo3yzIlpajMLq/nhi0j/w+P+nez4OKZtn0Wdd5uVTi7oQDPVCl/WbxpNlu4/rq9ZtSW4xCaChY9ZQCv6IZHznLJLFNoOD4j9CuM1gdB04t89/1O/w1cDnyilFU=}'
     }
-    var ChannelAccessToken = 'UfC+kvnTY/FnCX4xlcvUS6rJpw5mPeqHw8inmF+He1FKVxAYvpo3yzIlpajMLq/nhi0j/w+P+nez4OKZtn0Wdd5uVTi7oQDPVCl/WbxpNlu4/rq9ZtSW4xCaChY9ZQCv6IZHznLJLFNoOD4j9CuM1gdB04t89/1O/w1cDnyilFU=';
-    request({
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer {${ChannelAccessToken}}'
-        },
-        url: 'https://api.line.me/v2/bot/message/push',
-        method: 'POST',
-        body: data,
-        json: true
-    }, function (err, res, body) {
-        if (err) console.log('error')
-        if (res) console.log('success')
-        if (body) console.log(body)
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [{
+            type: 'text',
+            text: msg
+        }]
     })
-} 
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode);
+    });
+}
+
+
+// const request = require('request')
+// const port = process.env.PORT || 4000
+// app.post('/webhook', (req, res) => {
+//     var text = req.body.events[0].message.text;
+//     var userId = req.body.events[0].source.userId;
+//     if (text == "Hello") {
+//         var msg = [{
+//             type: 'text',
+//             text: 'สวสัดี'
+//         }]; PushMessage(userId, text, msg);
+//     } res.sendStatus(200);
+// });
+// app.listen(port)
+
+// function PushMessage(userId, text, msg) {
+//     let data = {
+//         to: userId,
+//         messages: msg
+//     }
+//     var ChannelAccessToken = 'UfC+kvnTY/FnCX4xlcvUS6rJpw5mPeqHw8inmF+He1FKVxAYvpo3yzIlpajMLq/nhi0j/w+P+nez4OKZtn0Wdd5uVTi7oQDPVCl/WbxpNlu4/rq9ZtSW4xCaChY9ZQCv6IZHznLJLFNoOD4j9CuM1gdB04t89/1O/w1cDnyilFU=';
+//     request({
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'Authorization': 'Bearer {${ChannelAccessToken}}'
+//         },
+//         url: 'https://api.line.me/v2/bot/message/push',
+//         method: 'POST',
+//         body: data,
+//         json: true
+//     }, function (err, res, body) {
+//         if (err) console.log('error')
+//         if (res) console.log('success')
+//         if (body) console.log(body)
+//     })
+// } 
