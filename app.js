61653c1f740a3f7b7510ee82ea3296850ba62c12
Taweesaksp <34016386+Taweesaksp@users.noreply.github.com>
2019-02-28 14:07:47 +0700

    linebotapi

--- app.js
+++ app.js
@@ -23,74 +23,37 @@
 //     .get('/', (req, res) => res.send(`Hi there! This is a nodejs-line-api running on PORT: ${PORT}`))
 //     .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-// const express = require('express')
-// const bodyParser = require('body-parser')
-// const request = require('request')
-// const app = express()
-// const port = process.env.PORT || 4000
-// app.use(bodyParser.urlencoded({ extended: false }))
-// app.use(bodyParser.json())
-// app.post('/webhook', (req, res) => {
-//     let reply_token = req.body.events[0].replyToken
-//     let msg = req.body.events[0].message.text
-//     reply(reply_token, msg)
-//     res.sendStatus(200)
-// })
-// app.listen(port)
-// function reply(reply_token, msg) {
-//     let headers = {
-//         'Content-Type': 'application/json',
-//         'Authorization': 'Bearer {UfC+kvnTY/FnCX4xlcvUS6rJpw5mPeqHw8inmF+He1FKVxAYvpo3yzIlpajMLq/nhi0j/w+P+nez4OKZtn0Wdd5uVTi7oQDPVCl/WbxpNlu4/rq9ZtSW4xCaChY9ZQCv6IZHznLJLFNoOD4j9CuM1gdB04t89/1O/w1cDnyilFU=}'
-//     }
-//     let body = JSON.stringify({
-//         replyToken: reply_token,
-//         messages: [{
-//             type: 'text',
-//             text: msg
-//         }]
-//     })
-//     request.post({
-//         url: 'https://api.line.me/v2/bot/message/reply',
-//         headers: headers,
-//         body: body
-//     }, (err, res, body) => {
-//         console.log('status = ' + res.statusCode);
-//     });
-// }
-
-/**
- * @author Pamontep Panya
- * @email pamontep.p@gmail.com
- * @create date 2018-06-03 03:45:02
- * @modify date 2018-06-03 03:45:02
- * @desc A sample project of Node.js and Line API
-*/
-const server = require('express');
-const PORT = process.env.PORT || 4000;
-const request = require('request');
-const bodyParser = require('body-parser');
-const lineMessaging = require('./src/classes/line-messaging');
-
-server()
-    .use(bodyParser.json())
-    .use(bodyParser.urlencoded({ extended: false }))
-    .get('/', (req, res) => res.send(`Hi there! This is a nodejs-line-api running on PORT: ${PORT}`))
-    // เพิ่มส่วนของ Webhook เข้าไป
-    .post('/webhook', function (req, res) {
-        let replyToken = req.body.events[0].replyToken;
-        let message = req.body.events[0].message.text;
-
-        console.log(`Message token : ${replyToken}`);
-        console.log(`Message from chat : ${message}`);
-
-        lineMessaging.replyMessage(replyToken, message).then(function (rs) {
-
-            console.log(`Reply message result : ${rs}`);
-
-            res.json({
-                status: 200,
-                message: `Sent message!`
-            });
-        });
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+const port = process.env.PORT || 4000
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.post('/webhook', (req, res) => {
+    let reply_token = req.body.events[0].replyToken
+    let msg = req.body.events[0].message.text
+    reply(reply_token, msg)
+    res.sendStatus(200)
+})
+app.listen(port)
+function reply(reply_token, msg) {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer {UfC+kvnTY/FnCX4xlcvUS6rJpw5mPeqHw8inmF+He1FKVxAYvpo3yzIlpajMLq/nhi0j/w+P+nez4OKZtn0Wdd5uVTi7oQDPVCl/WbxpNlu4/rq9ZtSW4xCaChY9ZQCv6IZHznLJLFNoOD4j9CuM1gdB04t89/1O/w1cDnyilFU=}'
+    }
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [{
+            type: 'text',
+            text: msg
+        }]
     })
-    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode);
+    });
+}
