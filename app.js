d856465451be6cb05c1eb9347e3d5a7dd36b7b4e
Taweesaksp <34016386+Taweesaksp@users.noreply.github.com>
2019-03-13 18:01:20 +0700

    linewe

--- app.js
+++ app.js
@@ -59,77 +59,25 @@
 // }
 
 
-const request = require('request')
-const port = process.env.PORT || 4000
-app.post('/webhook', function (req, res) {
-    var text = req.body.events[0].message.text;
-    var userId = "U93f0ab5384c81496cb14b0de52af58e9";
-    if (text == "Hello") {
-        var msg = [{
-            type: 'text',
-            text: 'สวัสดี'
-        }];
-        PushMessage(userId, text, msg);
-    } res.sendStatus(200);
-});
-app.listen(port)
-
-function PushMessage(userId, text, msg) {
-    let data = {
-        to: userId,
-        messages: msg
-    }
-
-    request({
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer {UfC+kvnTY/FnCX4xlcvUS6rJpw5mPeqHw8inmF+He1FKVxAYvpo3yzIlpajMLq/nhi0j/w+P+nez4OKZtn0Wdd5uVTi7oQDPVCl/WbxpNlu4/rq9ZtSW4xCaChY9ZQCv6IZHznLJLFNoOD4j9CuM1gdB04t89/1O/w1cDnyilFU=}'
-        },
-        url: 'https://api.line.me/v2/bot/message/push',
-        method: 'POST',
-        body: data,
-        json: true
-    }, function (err, res, body) {
-        if (err) console.log('error')
-        if (res) console.log('success')
-        if (body) console.log(body)
-    })
-}
-
-// const express = require('express')
-// const bodyParser = require('body-parser')
 // const request = require('request')
-// const app = express()
 // const port = process.env.PORT || 4000
-// app.use(bodyParser.urlencoded({ extended: false }))
-// app.use(bodyParser.json())
-// app.post('/webhook', (req, res) => {
-//     var text = req.body.events[0].message.text
-//     let reply_token = req.body.events[0].replyToken
-//     if (text == "มิวสิค") {
-//         reply(reply_token);
-//     }
-//     // var text = "88";
-//     // var userId = "U93f0ab5384c81496cb14b0de52af58e9";
-//     // let reply_token = req.body.events[0].replyToken
-//     // let msg = "99"
-//     PushMessage();
-//     res.sendStatus(200)
-// })
+// app.post('/webhook', function (req, res) {
+//     var text = req.body.events[0].message.text;
+//     var userId = "U93f0ab5384c81496cb14b0de52af58e9";
+//     if (text == "Hello") {
+//         var msg = [{
+//             type: 'text',
+//             text: 'สวัสดี'
+//         }];
+//         PushMessage(userId, text, msg);
+//     } res.sendStatus(200);
+// });
 // app.listen(port)
-// function PushMessage() {
+
+// function PushMessage(userId, text, msg) {
 //     let data = {
-//         "to": "U93f0ab5384c81496cb14b0de52af58e9",
-//         "messages": [
-//             {
-//                 "type": "text",
-//                 "text": "Friend"
-//             },
-//             {
-//                 "type": "text",
-//                 "text": "ทุกถ้อยคำพูดจาอ่อนหวาน..."
-//             }
-//         ]
+//         to: userId,
+//         messages: msg
 //     }
 
 //     request({
@@ -148,30 +96,82 @@ function PushMessage(userId, text, msg) {
 //     })
 // }
 
-// function reply(reply_token) {
-//     let headers = {
-//         'Content-Type': 'application/json',
-//         'Authorization': 'Bearer {UfC+kvnTY/FnCX4xlcvUS6rJpw5mPeqHw8inmF+He1FKVxAYvpo3yzIlpajMLq/nhi0j/w+P+nez4OKZtn0Wdd5uVTi7oQDPVCl/WbxpNlu4/rq9ZtSW4xCaChY9ZQCv6IZHznLJLFNoOD4j9CuM1gdB04t89/1O/w1cDnyilFU=}'
-//     }
-//     let body = {
-//         "replyToken": reply_token,
-//         "messages": [
-//             {
-//                 "type": "text",
-//                 "text": "Bnk"
-//             },
-//         ]
-//     }
-//     request.post({
-//         headers: headers,
-//         url: 'https://api.line.me/v2/bot/message/reply',
-//         body: body
-//     }, function (err, res, body) {
-//         if (err) console.log('error')
-//         if (res) console.log('success')
-//         if (body) console.log(body)
-//     })
-// }
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+const port = process.env.PORT || 4000
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.post('/webhook', (req, res) => {
+    var text = req.body.events[0].message.text
+    let reply_token = req.body.events[0].replyToken
+    if (text == "มิวสิค") {
+        reply(reply_token);
+    }
+    // var text = "88";
+    // var userId = "U93f0ab5384c81496cb14b0de52af58e9";
+    // let reply_token = req.body.events[0].replyToken
+    // let msg = "99"
+    PushMessage();
+    res.sendStatus(200)
+})
+app.listen(port)
+function PushMessage() {
+    let data = {
+        "to": "U93f0ab5384c81496cb14b0de52af58e9",
+        "messages": [
+            {
+                "type": "text",
+                "text": "Friend"
+            },
+            {
+                "type": "text",
+                "text": "ทุกถ้อยคำพูดจาอ่อนหวาน..."
+            }
+        ]
+    }
+
+    request({
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer {UfC+kvnTY/FnCX4xlcvUS6rJpw5mPeqHw8inmF+He1FKVxAYvpo3yzIlpajMLq/nhi0j/w+P+nez4OKZtn0Wdd5uVTi7oQDPVCl/WbxpNlu4/rq9ZtSW4xCaChY9ZQCv6IZHznLJLFNoOD4j9CuM1gdB04t89/1O/w1cDnyilFU=}'
+        },
+        url: 'https://api.line.me/v2/bot/message/push',
+        method: 'POST',
+        body: data,
+        json: true
+    }, function (err, res, body) {
+        if (err) console.log('error')
+        if (res) console.log('success')
+        if (body) console.log(body)
+    })
+}
+
+function reply(reply_token) {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer {UfC+kvnTY/FnCX4xlcvUS6rJpw5mPeqHw8inmF+He1FKVxAYvpo3yzIlpajMLq/nhi0j/w+P+nez4OKZtn0Wdd5uVTi7oQDPVCl/WbxpNlu4/rq9ZtSW4xCaChY9ZQCv6IZHznLJLFNoOD4j9CuM1gdB04t89/1O/w1cDnyilFU=}'
+    }
+    let body = {
+        "replyToken": reply_token,
+        "messages": [
+            {
+                "type": "text",
+                "text": "Bnk"
+            },
+        ]
+    }
+    request.post({
+        headers: headers,
+        url: 'https://api.line.me/v2/bot/message/reply',
+        body: body
+    }, function (err, res, body) {
+        if (err) console.log('error')
+        if (res) console.log('success')
+        if (body) console.log(body)
+    })
+}
 
 // var express = require('express')
 // var bodyParser = require('body-parser')
